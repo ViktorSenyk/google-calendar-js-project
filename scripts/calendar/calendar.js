c435930ac5a4ba3af2fcc1899ed322ab575306da
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-11-14 22:03:39 +0200

    add days-render

--- scripts/calendar/calendar.js
+++ scripts/calendar/calendar.js
@@ -1,11 +1,21 @@
 // import { getItem } from '../common/storage.js';
 // import { generateWeekRange } from '../common/time.utils.js';
 // import { renderEvents } from '../events/events.js';
-// import { createNumbersArray } from '../common/createNumbersArray.js';
+import { createNumbersArray } from '../common/createNumbersArray.js';
+
+const weekBlockElem = document.querySelector('.calendar__week');
 
 const generateDay = () => {
   // функция должна сгенерировать и вернуть разметку дня в виде строки
   // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
+  weekBlockElem.innerHTML = createNumbersArray(1, 7)
+    .map(
+      (day) =>
+        `<div class="calendar__day">${createNumbersArray(1, 24).map(
+          (min) => `<div class="calendar__time-slot" data-time="${min}"></div>`
+        ).join('')}</div>`
+    )
+    .join('');
 };
 
 export const renderWeek = () => {
@@ -14,4 +24,5 @@ export const renderWeek = () => {
   // массив дней, которые нужно отобразить, считаем ф-цией generateWeekRange на основе displayedWeekStart из storage
   // каждый день должен содержать в дата атрибуте порядковый номер дня в месяце
   // после того, как отрисовали всю сетку для отображаемой недели, нужно отобразить события этой недели с помощью renderEvents
+  generateDay();
 };
