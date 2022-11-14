ae9c40fde38fb4a95f1fc75e6952d1af6e3fd549
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-11-14 14:16:50 +0200

    add styles for header

--- scripts/common/modal.js
+++ scripts/common/modal.js
@@ -2,5 +2,11 @@ const modalElem = document.querySelector('.modal');
 const modalContentElem = document.querySelector('.modal__content');
 
 // опишите ф-ции openModal и closeModal
+export const openModal = () => {
+
+}
+export const closeModal = () => {
+    
+};
 // модальное окно работает похожим на попап образом
 // отличие в том, что попап отображается в месте клика, а модальное окно - по центру экрана
